4aac73adbea59f47ee8508deff23fabb8bb3cb60
Kevin Putnam <kevin.putnam@intel.com>
2018-12-13 14:15:11 -0800

    improved formatting and addition of whitelist to broken link checking

--- source/parse-link-check.py
+++ source/parse-link-check.py
@@ -5,6 +5,7 @@ import re
 
 fileName = "output.txt"
 outFile = "broken_links.html"
+whitelistFile = "link-whitelist.txt"
 
 if len(sys.argv) < 2:
     print ("Enter a file name to parse")
@@ -12,20 +13,48 @@ if len(sys.argv) < 2:
 
 outputPath = sys.argv[1]
 fileNamePath = outputPath + "/" + fileName
-outFilePath = outputPath + "/" + outFile
+outFilePath = outputPath + outFile
+
+with open (whitelistFile) as w:
+	whLines = w.readlines()
+
+whitelist = []
+for line in whLines:
+	link = line.rstrip()
+	whitelist.append(link)
 
 with open (fileNamePath) as f:
     lines = f.readlines()
 
 numBrokenLinks = 0
-newLines = []
+numWhiteListMatches = 0
+newLines = ["<!DOCTYPE html><html><head><style>body {font-family: sans-serif;}</style></head><body>"]
+whiteListLines = []
+
 for line in lines:
     if "[broken]" in line:
         strings = line.split(" ")
-        newLines.append("<b>" + strings[0] + "</b>\n<blockquote><a href=\"" + strings[2][:-1] + "\">[broken] " + strings[2][:-1] + "</a></blockquote>\n")
-        numBrokenLinks += 1
-newLines.insert(0,"<h1>" + str(numBrokenLinks) + " broken links found in Sphinx link check.</h1>\n")
+        link = strings[2][:-1]
+        link = link.strip()
+        if link in whitelist:
+            whiteListLines.append("<b>" + strings[0] + "</b>\n<blockquote><a href=\"" + link + "\">[whitelist] " + link + "</a></blockquote>\n")
+            numWhiteListMatches += 1
+        else:
+            newLines.append("<b>" + strings[0] + "</b>\n<blockquote><a href=\"" + link + "\">[broken] " + link + "</a></blockquote>\n")
+            numBrokenLinks += 1
+
+newLines.insert(0,"<h1>" + str(numBrokenLinks + numWhiteListMatches) + " broken links found in Sphinx link check</h1>\n")
+newLines.insert(1,"<h2>" + str(numBrokenLinks) + " unmatched broken links</h2>\n")
+newLines.append("<h2>" + str(numWhiteListMatches) + " links matched whitelist</h2>\n")
+for line in whiteListLines:
+	newLines.append(line)
+newLines.append("</body></html>")
 
 with open (outFilePath, "w") as outF:
     for line in newLines:
         outF.write(line)
+
+print("See ./" + outFilePath + " for a detailed breakdown of broken links.")
+
+if numBrokenLinks != 0:
+	sys.exit(-1) 
