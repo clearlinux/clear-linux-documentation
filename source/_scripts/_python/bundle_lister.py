d2396a6b4f6274abb5b5aef1717fb6c9be0ae346
michael vincerra <37549381+mvincerx@users.noreply.github.com>
2019-08-14 10:17:20 -0700

    Bundle lister update (#725)
    * Revises to simplify and refine code:
    - Removes regex PATTERN4 and 5
    - Revises PATTERN3 to exclude comments on custom additions
    - Removes pundler() because no longer a clr-bundles directory
    
    Signed-off-by: Michael Vincerra <michael.vincerra@intel.com>
    
    Removes cruft code.
    
    Signed-off-by: Michael Vincerra <michael.vincerra@intel.com>
    
    Edit for rebase.
    
    Signed-off-by: Michael Vincerra <michael.vincerra@intel.com>
    
    * Removes decode() for string in extractor().
    
    Signed-off-by: Michael Vincerra <michael.vincerra@intel.com>
    
    * Applies .bundledesc css to <p> tag on includes().
    
    Signed-off-by: Michael Vincerra <michael.vincerra@intel.com>
    
    * Modifies html tags in template and styles.css.
    
    Signed-off-by: Michael Vincerra <michael.vincerra@intel.com>

--- source/_scripts/_python/bundle_lister.py
+++ source/_scripts/_python/bundle_lister.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import io
 import os
 import re
@@ -9,13 +11,10 @@ from operator import itemgetter
 from datetime import datetime
 
 GITHUB_BASE = "https://github.com/clearlinux/clr-bundles/tree/master/bundles/"
-PUNDLES = "https://github.com/clearlinux/clr-bundles/blob/master/packages"
 
 PATTERN1 = re.compile(r"#\s?\[TITLE]:\w?(.*)")
 PATTERN2 = re.compile(r"#\s?\[DESCRIPTION]:\w?(.*)")
-PATTERN3 = re.compile(r"\(([^()]*|include)\)", re.MULTILINE)
-PATTERN4 = re.compile(r"^((?:(?!#)\w+[^-\s][-])\w+|\w+[^\s-])", re.MULTILINE)
-PATTERN5 = re.compile(r"^\w.+\s[#]\s(\w?.*)?", re.MULTILINE)
+PATTERN3 = re.compile(r"(?<=include)\(.*\)", re.MULTILINE)
 
 def extractor(lines):
     bundle_title = "title"
@@ -41,32 +40,6 @@ def extractor(lines):
             include_unique = set(include_list)
     return {"title": bundle_title, "data_desc": data_desc, "include_list": include_unique, "url": url}
 
-def pundler():
-    with io.open("./cloned_repo/clr-bundles/packages") as file_obj:
-        lines = file_obj.readlines()
-        pundle_title = "pundle_title"
-        pundle_desc = "pundle_desc"
-        purl = "purl"
-        pundle_list = []
-        pun_desc = []
-        pundle_master = []
-
-        for i in lines:
-            pundle = PATTERN4.findall(i)
-            pundle_plus = PATTERN5.findall(i)
-
-            if pundle:
-                pundle_title = pundle[0]
-                pundle_list.append(pundle_title)
-
-            if pundle_plus:
-                pundle_desc = pundle_plus[0].strip("[]")
-                pun_desc.append(pundle_desc)
-
-        for pun, desc in zip(pundle_list, pun_desc):
-                pundle_master.append({"title": pun, "pun_desc": desc, "purl": PUNDLES})
-    return pundle_master
-
 def bundler():
     data = []
     try:
@@ -79,16 +52,12 @@ def bundler():
                 lines = file_obj.readlines()
                 data.append(extractor(lines))
 
-    pundle_master = pundler()
-    data = data + pundle_master
     filtered = list(filter(lambda x: x.get('title'), data))
     sortedData = sorted(filtered, key=lambda x:x['title'].lower())
-    #ALT sortedData2 = sorted(sortedData, key=itemgetter('title'))
     loader = jinja2.FileSystemLoader(searchpath='./')
     env = jinja2.Environment(loader=loader)
     template = env.get_template('template.html')
     template.globals['now'] = datetime.utcnow
-
     output = template.render(data=sortedData, now=datetime.utcnow())
     with io.open('bundles.html.txt', 'w') as file:
         file.write(output)
