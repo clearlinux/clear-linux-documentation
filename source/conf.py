d4529b126bb69078b8b3153c1208d36509e229b4
Kristal Dale <kristal.dale@intel.com>
2019-10-04 09:38:41 -0700

    Add custom 404 and 403 pages. (#841)
    - Added static html pages for 404 and 403 server responses.
    - Added conf.py setting html_extra_path to copy static _errors folder
      with static 404/403 files into build output
    
    Signed-off-by: Kristal Dale <kristal.dale@intel.com>

--- source/conf.py
+++ source/conf.py
@@ -178,7 +178,7 @@ html_favicon = '_images/favicon.ico'
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+html_extra_path = ['_html_extra']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
