a9e65386ccff58d95ee10a37d8d5c74508e802d3
Kevin Putnam <kevin.putnam@intel.com>
2020-04-08 14:18:39 -0700

    Moved to Makefile.
    Signed-off-by: Kevin Putnam <kevin.putnam@intel.com>

--- source/_scripts/_python/manpages/clone-man-repos.sh
+++
@@ -1,6 +0,0 @@
-git clone https://ithub.com/clearlinux/clr-man-pages.git
-git clone https://github.com/clearlinux/clr-power-tweaks.git
-git clone https://github.com/clearlinux/clrtrust.git
-git clone https://github.com/clearlinux/mixer-tools.git
-git clone https://github.com/clearlinux/swupd-client.git
-git clone https://github.com/clearlinux/telemetrics-client.git
