6032107a82df4873487cae34923d2035cd829190
Kevin Putnam <kevin.putnam@intel.com>
2020-04-08 12:53:39 -0700

    Change git URLs to https.
    Signed-off-by: Kevin Putnam <kevin.putnam@intel.com>

--- source/_scripts/_python/manpages/clone-man-repos.sh
+++ source/_scripts/_python/manpages/clone-man-repos.sh
@@ -1,6 +1,6 @@
-git clone git@github.com:clearlinux/clr-man-pages.git
-git clone git@github.com:clearlinux/clr-power-tweaks.git
-git clone git@github.com:clearlinux/clrtrust.git
-git clone git@github.com:clearlinux/mixer-tools.git
-git clone git@github.com:clearlinux/swupd-client.git
-git clone git@github.com:clearlinux/telemetrics-client.git
+git clone https://ithub.com/clearlinux/clr-man-pages.git
+git clone https://github.com/clearlinux/clr-power-tweaks.git
+git clone https://github.com/clearlinux/clrtrust.git
+git clone https://github.com/clearlinux/mixer-tools.git
+git clone https://github.com/clearlinux/swupd-client.git
+git clone https://github.com/clearlinux/telemetrics-client.git
