c3b0b6e4ab8cd3cd97e44f7bc881fe8406daef4d
Kevin Putnam <kevin.putnam@intel.com>
2020-04-14 13:16:49 -0700

    Updated setup.py with Clear Linux docs info.
    Signed-off-by: Kevin Putnam <kevin.putnam@intel.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='Sphinx GUI Utility',
       version='0.1',
       description='Build Sphinx docs from a GUI',
-      author='Kevin Putnam',
+      author='Many',
       author_email='kevin.putnam@intel.com',
-      url='https://gitlab.devtools.intel.com/kputnam/doc-gui/',
+      url='https://github.com/clearlinux/clear-linux-documentation/',
       )
