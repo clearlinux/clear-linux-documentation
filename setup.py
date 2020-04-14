bd4e1acf2bae46095b68c23b820d34346fba817a
Kevin Putnam <kevin.putnam@intel.com>
2020-04-14 13:11:03 -0700

    Adding support for reDocs GUI to Clear Linux Docs repo.
    1. Added .tox and MANIFEST to .gitignore
    2. Added py rule to make.bat to enable building of bundles.html.txt in Windows
    3. Added .tox to exclude_patterns in conf.py
    4. Added tox.ini and setup.py support files to project.
    
    Signed-off-by: Kevin Putnam <kevin.putnam@intel.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sphinx GUI Utility',
+      version='0.1',
+      description='Build Sphinx docs from a GUI',
+      author='Kevin Putnam',
+      author_email='kevin.putnam@intel.com',
+      url='https://gitlab.devtools.intel.com/kputnam/doc-gui/',
+      )
--- source/conf.py
+++ source/conf.py
@@ -81,7 +81,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build','_themes']
+exclude_patterns = ['_build','_themes','.tox']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
